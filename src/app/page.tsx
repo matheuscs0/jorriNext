14cb300d132cf10c5ae299cc6866949548876d0a
Matheus Silva <dev.matheuscs@gmail.com>
2023-10-14 12:36:40 -0300

    API products

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,7 @@ export default function Home() {
   const {toggleCart, cartOpen} = HandlesSideBar()
 
   const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([])
 
   async function getApi() {
     const response = await axios.get("http://localhost:3002/api/products");
@@ -30,10 +31,7 @@ export default function Home() {
       />
       <div className="flex flex-wrap justify-center items-center gap-5 p-5">
         {loading ? <Loading/> :
-        <ProductCard 
-          name="Pulseira Prata ELo"
-          price={150.32}
-          hasBannerImage="/logoJorri.png"
+        <ProductCard
         />
        }
       </div>
--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -2,36 +2,54 @@ import Link from "next/link";
 import { ButtonLink } from "../Buttons/ButtonLink";
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { Loading } from "../Loading";
 
-type ProductCardProps = {
+// Defina tipos para as propriedades e para o estado
+type Product = {
+  id: number;
   name: string;
   price: number;
-  hasBannerImage: string;
-};
+}
 
-const ProductCard = ({ name, price, hasBannerImage }: ProductCardProps) => {
+const ProductCard: React.FC = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  async function getApi() {
+    try {
+      const response = await axios.get<Product[]>("http://localhost:3002/api/products");
+      const data = response.data;
+      setProducts(data);
+    } catch (error) {
+      console.error("Erro ao obter dados da API:", error);
+    }
+  }
+
+  useEffect(() => {
+    getApi();
+  }, []);
 
   return (
     <Link href="/">
-        <div className="flex flex-col w-[308px] h-[410px] bg-zinc-100 justify-around items-center p-5 rounded-2xl shadow-md cursor-pointer">
+      {products.map((product) => (
+        <div className="flex flex-col w-[308px] h-[410px] bg-zinc-100 justify-around items-center p-5 rounded-2xl shadow-md cursor-pointer" key={product.id}>
           <div className="flex w-full h-[250px] justify-center items-center">
             <img
-              src={hasBannerImage}
-              alt={name}
+              src="logoJorri.png"
+              alt={product.name}
               className="flex w-full h-full object-contain"
             />
           </div>
           <div className="flex flex-col justify-center items-center gap-2 w-full">
-            <h3 className="text-center text-lg font-medium">{name}</h3>
-            <p>R$ {price}</p>
+            <h3 className="text-center text-lg font-medium">{product.name}</h3>
+            <p>R$ {product.price}</p>
           </div>
           <div className="w-full flex justify-center items-center">
             <ButtonLink href="/id">Comprar</ButtonLink>
           </div>
         </div>
+      ))}
     </Link>
   );
 };
 
 export { ProductCard };
+
