71238550ab5b87dedbcaa8ed2c98dd86373245c2
Matheus Silva <dev.matheuscs@gmail.com>
2023-12-11 19:04:39 -0300

    funciona

--- src/app/purchase/page.tsx
+++ src/app/purchase/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { useEffect, useState } from 'react';
 import { TableTotal } from "@/components/TableTotal";
 import { Table } from "../Table";
--- src/app/purchase/payment/page.tsx
+++ src/app/purchase/payment/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { useEffect, useState } from 'react';
 import { PaymentMethods } from "@/components/PaymentMethods";
 import { PaymentOrder } from "@/components/PaymentOrder";
