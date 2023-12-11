7cd6aaa78258a502efb80d807e8428b64c3c75a4
Matheus Silva <dev.matheuscs@gmail.com>
2023-12-11 18:54:28 -0300

    finish

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -43,7 +43,7 @@ export default function SearchPage() {
           ))
         ) : (
           <div className="mt-10">
-            <Loading />
+            <Loading size={32}/>
           </div>
         )}
       </section>
