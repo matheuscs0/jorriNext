9022e19fefbc3ed606d26a5ab356908d979e68a9
Matheus Silva <dev.matheuscs@gmail.com>
2023-12-11 19:02:54 -0300

    finish01

--- src/app/purchase/page.tsx
+++ src/app/purchase/page.tsx
@@ -1,15 +1,34 @@
-'use client'
+import { useEffect, useState } from 'react';
 import { TableTotal } from "@/components/TableTotal";
 import { Table } from "../Table";
 import { ShippingFrete } from "@/components/shippingFrete";
 import { TableTotalMobile } from "@/components/TableTotal/TableTotalMobile";
 
 export default function PurchasePage() {
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        // Verificar o tamanho da janela no lado do cliente
+        const handleResize = () => {
+            setIsMobile(window.innerWidth < 750);
+        };
+
+        // Adicionar o ouvinte de redimensionamento
+        window.addEventListener('resize', handleResize);
+
+        // Chamar handleResize inicialmente para definir o estado inicial
+        handleResize();
+
+        // Remover o ouvinte de redimensionamento ao desmontar o componente
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []); // Executar apenas uma vez durante a montagem do componente
 
     return (
         <section className="w-full mt-24 h-full flex gap-20  flex-col">
-            <div className={`w-full gap-20 flex justify-center ${window.innerWidth < 750 ? 'flex-col' : ''}`}>
-                <div className={`${window.innerWidth < 750 ? 'hidden' : 'flex'}`}>
+            <div className={`w-full gap-20 flex justify-center ${isMobile ? 'flex-col' : ''}`}>
+                <div className={`${isMobile ? 'hidden' : 'flex'}`}>
                     <Table.Root>
                         <Table.Header/>
                         <Table.Body/>
@@ -18,10 +37,10 @@ export default function PurchasePage() {
                     <TableTotal/>
                 </div>
             
-                <div className={`${window.innerWidth < 750 ? 'flex' : 'hidden'}`}>
+                <div className={`${isMobile ? 'flex' : 'hidden'}`}>
                     <TableTotalMobile/>
                 </div>
             </div>
         </section>
-    )
+    );
 }
--- src/app/purchase/payment/page.tsx
+++ src/app/purchase/payment/page.tsx
@@ -1,23 +1,38 @@
-'use client'
+import { useEffect, useState } from 'react';
 import { PaymentMethods } from "@/components/PaymentMethods";
 import { PaymentOrder } from "@/components/PaymentOrder";
 import { SearchCep } from "@/components/searchCep";
 import { useCardFormContext } from "@/contexts/CardFormContext";
 
 export default function PaymentPage() {
-    return(
-        <div className={`w-full flex justify-around items-center ${window.innerWidth < 650 ? 'flex-col mt-[1000px]' : ''}`}>
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth < 650);
+        };
+
+        window.addEventListener('resize', handleResize);
+        handleResize();
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    return (
+        <div className={`w-full flex justify-around items-center ${isMobile ? 'flex-col mt-[1000px]' : ''}`}>
             <div className={`gap-2 flex-col justify-center`}>
                 <div className="w-full flex justify-center mb-5">
-                    <SearchCep/>
+                    <SearchCep />
                 </div>
-                <div  className="w-full flex justify-center mb-5">
-                    <PaymentMethods/>
+                <div className="w-full flex justify-center mb-5">
+                    <PaymentMethods />
                 </div>
             </div>
             <div className={`flex justify-center`}>
-                <PaymentOrder/>
+                <PaymentOrder />
             </div>
         </div>
-    )
+    );
 }
